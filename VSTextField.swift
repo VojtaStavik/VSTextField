c6867d73b8231c6a18843db3dfaca5d3da1c61a3
Vojta Stavik <vojta@ulikeit.com>
2015-04-15 15:55:47 +0200

    Add formattedSecureTextEntry option, syntax improvements

--- VSTextField.swift
+++ VSTextField.swift
@@ -11,7 +11,7 @@ enum TextFieldFormatting {
     
     case SocialSecurityNumber
     case Custom
-    case Default
+    case NoFormatting
 }
 
 
@@ -32,6 +32,12 @@ class VSTextField: UITextField {
     A character which will be replaced in formattingPattern by a number
     */
     var replacementChar: Character = "*"
+
+    
+    /**
+    A character which will be replaced in formattingPattern by a number
+    */
+    var secureTextReplacementChar: Character = "\u{25cf}"
     
     
     /**
@@ -42,9 +48,9 @@ class VSTextField: UITextField {
     
     
     /**
-    Type of predefined text formatting. You don't have to set this. (It's more a future feature)
+    Type of predefined text formatting. (You don't have to set this. It's more a future feature)
     */
-    var formatting : TextFieldFormatting = .Default {
+    var formatting : TextFieldFormatting = .NoFormatting {
         
         didSet {
             
@@ -76,16 +82,20 @@ class VSTextField: UITextField {
 
     
     /**
-    Text without formatting characters (read-only)
+    Provides secure text entry but KEEPS formatting. All digits are replaced with the bullet character \u{25cf} .
     */
-    var textWithoutFormatting: String? {
+    var formatedSecureTextEntry: Bool {
         
-        if let text = self.text {
+        set {
             
-            return VSTextField.makeOnlyDigitsString(text)
+            _formatedSecureTextEntry = newValue
+            super.secureTextEntry = false
         }
         
-        return nil
+        get {
+            
+            return _formatedSecureTextEntry
+        }
     }
     
     
@@ -94,7 +104,7 @@ class VSTextField: UITextField {
         set {
             
             super.text = newValue
-            textDidChange() // format string properly even when it's set programatically
+        //    textDidChange() // format string properly even when it's set programatically
         }
         
         get { return super.text }
@@ -118,7 +128,15 @@ class VSTextField: UITextField {
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
     
-
+    /**
+    Final text without formatting characters (read-only)
+    */
+    var finalStringWithoutFormatting : String {
+        
+        return VSTextField.makeOnlyDigitsString(_textWithoutSecureBullets)
+    }
+    
+    
     // MARK: - class methods
     
     class func makeOnlyDigitsString(string: String) -> String {
@@ -128,26 +146,15 @@ class VSTextField: UITextField {
     
     
     
-    // MARK: - internal
+    // MARK: - INTERNAL
     
-    private func numberOfDigitsInFormattingString(string: String) -> Int {
-
-        var number: Int = 0
-        
-        for i in 0...max(0, count(string) - 1) {
-            
-            let range = Range<String.Index>(start: advance(string.startIndex, i), end: advance(string.startIndex, i + 1 ))
-            
-            if string.substringWithRange(range) == String(replacementChar) {
-                
-                number++
-            }
-        }
-        
-        return number
-    }
-
-
+    private var _formatedSecureTextEntry = false
+    
+    // if secureTextEntry is false, this value is similar to self.text
+    // if secureTextEntry is true, you can find final formatted text without bullets here
+    private var _textWithoutSecureBullets = ""
+    
+    
     private func registerForNotifications() {
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "textDidChange", name: "UITextFieldTextDidChangeNotification", object: self)
@@ -156,17 +163,29 @@ class VSTextField: UITextField {
     
     func textDidChange() {
         
-        if formatting == .Default {
+        log.message("text did change")
+        
+        // TODO: - Isn't there more elegant way how to do this?
+        let currentTextForFormatting: String
+        
+        if count(text) > count(_textWithoutSecureBullets) {
             
-            return;
+            currentTextForFormatting = _textWithoutSecureBullets + text.substringFromIndex(advance(text.startIndex, count(_textWithoutSecureBullets)))
+        }
+        
+        else {
+            
+            currentTextForFormatting = _textWithoutSecureBullets.substringToIndex(advance(_textWithoutSecureBullets.startIndex, count(text)))
         }
         
         
-        if let textFieldString = super.text where formattingPattern != "" {
+        if formatting != .NoFormatting && count(currentTextForFormatting) > 0 && count(formattingPattern) > 0 {
             
-            let tempString = VSTextField.makeOnlyDigitsString(textFieldString)
+            let tempString = VSTextField.makeOnlyDigitsString(currentTextForFormatting)
             
             var finalText = ""
+            var finalSecureText = ""
+            
             var stop = false
             
             var formatterIndex = formattingPattern.startIndex
@@ -179,37 +198,51 @@ class VSTextField: UITextField {
                 if formattingPattern.substringWithRange(formattingPatternRange) != String(replacementChar) {
                     
                     finalText = finalText.stringByAppendingString(formattingPattern.substringWithRange(formattingPatternRange))
+                    finalSecureText = finalSecureText.stringByAppendingString(formattingPattern.substringWithRange(formattingPatternRange))
                 }
-                
+                    
                 else if count(tempString) > 0 {
                     
                     let pureStringRange = Range(start: tempIndex, end: advance(tempIndex, 1))
                     
                     finalText = finalText.stringByAppendingString(tempString.substringWithRange(pureStringRange))
+                    
+                    // we want the last number to be visible
+                    if advance(tempIndex, 1) == tempString.endIndex {
+                        
+                        finalSecureText = finalSecureText.stringByAppendingString(tempString.substringWithRange(pureStringRange))
+                    }
+                    
+                    else {
+                        
+                        finalSecureText = finalSecureText.stringByAppendingString(String(secureTextReplacementChar))
+                    }
+                    
                     tempIndex++
                 }
                 
                 formatterIndex++
                 
                 if formatterIndex >= formattingPattern.endIndex || tempIndex >= tempString.endIndex {
-
+                    
                     stop = true
                 }
             }
             
-            super.text = finalText
+            _textWithoutSecureBullets = finalText
+            super.text = _formatedSecureTextEntry ? finalSecureText : finalText
         }
         
-        
+        // Let's check if we have additional max length restrictions
         if maxLenght > 0 {
             
-            if let text = self.text {
-            
-                if count(text) > maxLenght {
-                    
-                    self.text = self.text.substringToIndex(advance(self.text.startIndex, maxLenght))
-                }
+            if count(text) > maxLenght {
+                
+                super.text = text.substringToIndex(advance(text.startIndex, maxLenght))
+                _textWithoutSecureBullets = _textWithoutSecureBullets.substringToIndex(advance(_textWithoutSecureBullets.startIndex, maxLenght))
             }
         }
+        
+        log.message("final text: \(self.text) \n\n")
     }
 }
