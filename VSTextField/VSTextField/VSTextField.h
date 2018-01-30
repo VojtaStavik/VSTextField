ef818269386b48712842b66fd3b071618aaa009f
Munir Wanis <fallen.angel.pp@gmail.com>
2018-01-30 12:40:55 -0200

    ➕  Adds new project of type Cocoa Touch Framework so we can use on Pods and Carthage

---
+++ VSTextField/VSTextField/VSTextField.h
@@ -0,0 +1,19 @@
+//
+//  VSTextField.h
+//  VSTextField
+//
+//  Created by Munir Wanis on 30/01/18.
+//  Copyright © 2018 Munir Wanis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VSTextField.
+FOUNDATION_EXPORT double VSTextFieldVersionNumber;
+
+//! Project version string for VSTextField.
+FOUNDATION_EXPORT const unsigned char VSTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VSTextField/PublicHeader.h>
+
+
